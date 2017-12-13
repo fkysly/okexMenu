7dad63ce0c475b72501e271f66266f241a56749c
马天翼 <matianyi@laiqutech.com>
2017-12-13 11:41:30 +0800

    update v1.0.0

---
+++ app/main.js
@@ -0,0 +1,44 @@
+const {app, Menu, Tray} = require('electron')
+const WebSocket = require('ws')
+
+let tray = null
+app.on('ready', () => {
+  tray = new Tray('/Users/matianyi/work/okexmenu/app/logo.png')
+  const contextMenu = Menu.buildFromTemplate([
+    {label: 'Quit', type: 'normal', click: () => {
+      app.quit()
+    }}
+  ])
+  tray.setTitle('')
+  tray.setContextMenu(contextMenu)
+
+  okex((price) => {
+    tray.setTitle("iota: " + price)
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+function okex(callback) {
+  let ws = new WebSocket('wss://real.okex.com:10440/websocket/okexapi')
+  ws.on('open', () => {
+    ws.send("{'event':'addChannel','channel':'ok_sub_spot_iota_usdt_ticker'}")
+  })
+  
+  ws.on('message', (msg) => {
+    var data = JSON.parse(msg)
+    data = data[0].data
+    if (data.last) {
+      let price = data.last
+      callback(price)
+    }
+  })
+  
+  ws.on('close', () => {
+    okex()
+  })
+}
